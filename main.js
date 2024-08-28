6050cecb48c02c3716ef2f58528348ae00414070
AntonioGM66 <oitog3@gmail.com>
2024-08-27 19:35:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/*for(let i=1; i<=50; i++)
+{
+    if(i%2!=0)
+    {
+        console.log(i);
+    }
+}
+
+let array = [4, "dos", 8, "tres", 5, 9, 1, "cero"];
+
+for (let i = 0; i < array.length; i++) 
+{
+    // Verificar si el elemento es de tipo número
+    if (typeof array[i] === "number") 
+    {
+        console.log(array[i]);
+    }
+}*/
